3d468c5eff1bee4d7a968216c4045db53e53c243
Sergey Mishin <sergei.a.mishin@gmail.com>
2018-04-25 13:18:40 +0300

    add ex-3.7

---
+++ donovan-tgpl/ch3/ex-3.7/main.go
@@ -0,0 +1,28 @@
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"image"
+	"image/png"
+	"os"
+
+	"github.com/qezz/go-course/donovan-tgpl/ch3/ex-3.6/mandelbrot"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		for px := 0; px < width; px++ {
+			ssp := mandelbrot.SupersampledPixel(2, width, height, px, py)
+
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, ssp)
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
